597963b154a3f1af44400cd9eff092c7f1563783
Sanjay <svamja@gmail.com>
2015-09-26 19:42:06 +0530

    do not take screenshot on user being idle

--- minute.tpl.sh
+++ minute.tpl.sh
@@ -6,9 +6,23 @@ then
     exit;
 fi;
 
-date
-DISPLAY=:0 scrot $IMAGE_PATH/'mint-%Y-%m-%d_%H-%M-%S.jpg' -q $IMAGE_QUALITY
+idle_time=`DISPLAY=:0 xprintidle`
+if test "$idle_time" -gt 30000
+then
+    echo `date` idle
+else
+    echo `date` screenshot
+    DISPLAY=:0 scrot $IMAGE_PATH/'mint-%Y-%m-%d_%H-%M-%S.jpg' -q $IMAGE_QUALITY
+fi
+
 sleep 30
 
-date
-DISPLAY=:0 scrot $IMAGE_PATH/'mint-%Y-%m-%d_%H-%M-%S.jpg' -q $IMAGE_QUALITY
+idle_time=`DISPLAY=:0 xprintidle`
+if test "$idle_time" -gt 30000
+then
+    echo `date` idle
+else
+    echo `date` screenshot
+    DISPLAY=:0 scrot $IMAGE_PATH/'mint-%Y-%m-%d_%H-%M-%S.jpg' -q $IMAGE_QUALITY
+fi
+
